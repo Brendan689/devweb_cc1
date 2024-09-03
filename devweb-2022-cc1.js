db0c5b5985d1c74ac684c4d022487d1c633fb9d0
Brendan689 <taofilevi2@gmail.com>
2024-09-03 11:41:43 +1100

    Update devweb-2022-cc1.js

--- devweb-2022-cc1.js
+++ devweb-2022-cc1.js
@@ -51,14 +51,14 @@ function addCow(evt) {
   console.debug(evt.x, evt.y);
   "use strict";
 
-    const $cow = document.createElement("cow");
-    $cow.src = "https://upload.wikimedia.org/wikipedia/commons/3/30/Cowicon.svg"; 
-    $cow.className = "cow"; // ajoute la classe "cow" pour le style
-    $cow.style.left = `${evt.pageX}px`; // coordonnée x du clic
-    $cow.style.top = `${evt.pageY}px`; // coordonnée y du clic
+    const img = document.createElement("img");
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/3/30/Cowicon.svg"; 
+    img.className = "cow"; // ajoute la classe "cow" pour le style
+    img.style.left = `${evt.pageX}px`; // coordonnée x du clic
+    img.style.top = `${evt.pageY}px`; // coordonnée y du clic
     
     const rotation = Math.random() * 360; //  rotation de l'image
-    $cow.style.transform = `rotate(${rotation}deg)`;
+    img.style.transform = `rotate(${rotation}deg)`;
     
         // ajoute de l'image sur la page
     document.body.appendChild(cow);
